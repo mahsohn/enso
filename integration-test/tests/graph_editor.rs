f7d7e7f444368282545693c1b0797438285a78ab
Ilya Bogdanov <fumlead@gmail.com>
2022-02-23 18:57:58 +0300

    refactor test

--- integration-test/tests/graph_editor.rs
+++ integration-test/tests/graph_editor.rs
@@ -1,10 +1,11 @@
 use enso_integration_test::prelude::*;
 
 use approx::assert_abs_diff_eq;
+use enso_gui::view::graph_editor::NodeSource;
 use enso_web::sleep;
 use ensogl::display::navigation::navigator::ZoomEvent;
-use std::time::Duration;
 use ordered_float::OrderedFloat;
+use std::time::Duration;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
@@ -103,44 +104,43 @@ async fn zooming() {
 #[wasm_bindgen_test]
 async fn adding_node_with_add_node_button() {
     let test = IntegrationTestOnNewProject::setup().await;
-    let project = test.project_view();
     let graph_editor = test.graph_editor();
-
     let add_node_button = &graph_editor.model.add_node_button;
     let node_added = graph_editor.node_added.next_event();
-    
-    let nodes_and_positions = graph_editor.model.nodes.all.keys().into_iter().flat_map(|id| graph_editor.model.get_node_position(id).map(|pos| (id, pos)));
-    let mut nodes_sorted_by_y_axis = nodes_and_positions.sorted_by_key(|(_, pos)| OrderedFloat(pos.y));
-    let (_, bottom_most_pos) = nodes_sorted_by_y_axis.next().expect("Default project does not contain any nodes");
 
+    let nodes = graph_editor.model.nodes.all.keys();
+    let nodes_positions = nodes.into_iter().flat_map(|id| graph_editor.model.get_node_position(id));
+    let mut sorted_positions = nodes_positions.sorted_by_key(|pos| OrderedFloat(pos.y));
+    let bottom_most_pos =
+        sorted_positions.next().expect("Default project does not contain any nodes");
+
+    // Node is created below the bottom-most one.
     add_node_button.click();
-
     let (node_id, node_source) = node_added.expect();
     assert!(node_source.is_none());
     let node_position = graph_editor.model.get_node_position(node_id).expect("Node was not added");
-    // Node is created below the bottom-most one
-    assert!(node_position.y < bottom_most_pos.y, "Expected that {node_position} < {bottom_most_pos}");
+    assert!(
+        node_position.y < bottom_most_pos.y,
+        "Expected that {node_position} < {bottom_most_pos}"
+    );
 
+    // Selected node is used as a `source` node.
     graph_editor.model.nodes.deselect_all();
     graph_editor.model.nodes.select(node_id);
-    
     let node_added = graph_editor.node_added.next_event();
     add_node_button.click();
+    let (_, node_source) = node_added.expect();
+    assert_eq!(node_source, Some(NodeSource { node: node_id }));
 
-    let (node_id2, node_source) = node_added.expect();
-    assert_eq!(node_source, Some(NodeSource{ node: node_id}));
-
+    // If there is a free space, the node new node is created in the center of screen.
     let camera = test.ide.ensogl_app.display.scene().layers.main.camera();
-
     camera.mod_position_xy(|pos| pos + Vector2(1000.0, 1000.0));
-
     graph_editor.model.nodes.deselect_all();
     let node_added = graph_editor.node_added.next_event();
     add_node_button.click();
-    let (node_id3, node_source) = node_added.expect();
-    
-    let node_position = graph_editor.model.get_node_position(node_id3).expect("Node was not added");
-    let center_of_screen = test.ide.ensogl_app.display.scene().screen_to_scene_coordinates(Vector3(0.0, 0.0, 0.0));
+    let (node_id, _) = node_added.expect();
+    let node_position = graph_editor.model.get_node_position(node_id).expect("Node was not added");
+    let center_of_screen =
+        test.ide.ensogl_app.display.scene().screen_to_scene_coordinates(Vector3(0.0, 0.0, 0.0));
     assert_eq!(node_position.xy(), center_of_screen.xy());
 }
-
