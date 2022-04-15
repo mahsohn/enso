8c6886f39ffc0456a028a7a2fb76f4e0e861e516
Radosław Waśko <radoslaw.wasko@enso.org>
2022-04-15 13:36:12 +0200

    Check for sentinels regardless of AcceptsError

--- lib/scala/interpreter-dsl/src/main/java/org/enso/interpreter/dsl/MethodProcessor.java
+++ lib/scala/interpreter-dsl/src/main/java/org/enso/interpreter/dsl/MethodProcessor.java
@@ -125,11 +125,14 @@ public class MethodProcessor extends AbstractProcessor {
       for (MethodDefinition.ArgumentDefinition arg : methodDefinition.getArguments()) {
         if (arg.shouldCheckErrors()) {
           String condName = mkArgumentInternalVarName(arg) + DATAFLOW_ERROR_PROFILE;
-          String branchName = mkArgumentInternalVarName(arg) + PANIC_SENTINEL_PROFILE;
           out.println(
               "  private final ConditionProfile "
                   + condName
                   + " = ConditionProfile.createCountingProfile();");
+        }
+
+        if (arg.isPositional() && !arg.isThis()) {
+          String branchName = mkArgumentInternalVarName(arg) + PANIC_SENTINEL_PROFILE;
           out.println("  private final BranchProfile " + branchName + " = BranchProfile.create();");
         }
 
@@ -270,8 +273,8 @@ public class MethodProcessor extends AbstractProcessor {
 
   private void generateArgumentRead(
       PrintWriter out, MethodDefinition.ArgumentDefinition arg, String argsArray) {
+    String argReference = argsArray + "[" + arg.getPosition() + "]";
     if (arg.shouldCheckErrors()) {
-      String argReference = argsArray + "[" + arg.getPosition() + "]";
       String condProfile = mkArgumentInternalVarName(arg) + DATAFLOW_ERROR_PROFILE;
       out.println(
           "    if ("
@@ -283,9 +286,11 @@ public class MethodProcessor extends AbstractProcessor {
               + argReference
               + ");\n"
               + "    }");
+    }
+    if (!arg.isThis()) {
       String branchProfile = mkArgumentInternalVarName(arg) + PANIC_SENTINEL_PROFILE;
       out.println(
-          "    else if (TypesGen.isPanicSentinel("
+          "    if (TypesGen.isPanicSentinel("
               + argReference
               + ")) {\n"
               + "      "
@@ -366,7 +371,9 @@ public class MethodProcessor extends AbstractProcessor {
   private boolean generateWarningsCheck(
       PrintWriter out, List<MethodDefinition.ArgumentDefinition> arguments, String argumentsArray) {
     List<MethodDefinition.ArgumentDefinition> argsToCheck =
-        arguments.stream().filter(ArgumentDefinition::shouldCheckWarnings).collect(Collectors.toList());
+        arguments.stream()
+            .filter(ArgumentDefinition::shouldCheckWarnings)
+            .collect(Collectors.toList());
     if (argsToCheck.isEmpty()) {
       return false;
     } else {
